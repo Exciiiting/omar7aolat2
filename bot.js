886974ea742508016335ed1febe7f041ae2b7b9d
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-21 14:06:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = "#"
 
 
 client.on('message', function(message) {
-    const myID = "434445470946033665";
+    const myID = "511246372939366430";
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "sn")) {
                 if(message.author.id !== myID) return;
